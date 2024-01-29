27b3a6965985fa73fed046b1b291c44e41670aa6
ViiesTech <viistech.developer@gmail.com>
2024-01-30 04:18:55 +0500

    hello

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const { Server } = require("socket.io");
 const io = new Server(server);
 const cors = require('cors')
 const Route = require('./Routes.js');
+const User = require('./models/UserModel.js')
 
 const db = require('./DatabaseConnect');
 
@@ -18,20 +19,23 @@ db()
 
 app.get('/', (req, res) => {
     res.send('<H1>Kearb socket</H1>');
+
 });
 
 
 
 
 io.on('connection', (socket) => {
-  console.log('a user connected SocketChatApp', socket.id);
-  
-      User.create({ username, socketId: socket.id }, (err, user) => {
-      if (err) throw err;
-      console.log('User logged in:', user);
-    });
-    
-    
+    console.log('a user connected SocketChatApp', socket.id);
+
+    socket.on('login', (data) => {
+        // Save user details to MongoDB
+            console.log('User logged in:', data);
+        // User.create({ username, socketId: socket.id }, (err, user) => {
+        //     if (err) throw err;
+        // });
+    })
+
 });
 
 
